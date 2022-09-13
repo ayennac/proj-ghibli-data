4fbfc62f00fbd882e4bf3a518d5142ac41cdf28d
ayennac <ayennac@gmail.com>
2022-09-13 12:57:08 -0700

    added code to initialize map. showing up as tan

--- server.py
+++ server.py
@@ -3,17 +3,19 @@
 from flask import (Flask, render_template, request, flash, session,
                    redirect)
 
+import os
 from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+maps_api_key = os.environ['MAPS_API_KEY']
 
 @app.route('/homepage')
 def homepage():
     """Show homepage"""
 
-    return render_template("homepage.html")
+    return render_template("homepage.html", maps_api_key = maps_api_key)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
---
+++ static/map-script.js
@@ -0,0 +1,8 @@
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 1,
+    });
+  }
+  
+window.initMap = initMap;
