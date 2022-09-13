afb23478cc200d13de9025755987ee306d600503
ayennac <ayennac@gmail.com>
2022-09-13 14:13:38 -0700

    map initialized with center at uluru

--- static/map-script.js
+++ static/map-script.js
@@ -1,8 +1,13 @@
 
 function initMap() {
+
+    const uluru = { lat: -25.344, lng: 131.031 };
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 1,
+      zoom: 3,
+      center: uluru,
     });
+    //to create a map you must initialize with zoom and center
+
   }
   
 window.initMap = initMap;
