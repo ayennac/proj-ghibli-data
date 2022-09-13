49460a4306820906ee012d833651af5eec4828ef
ayennac <ayennac@gmail.com>
2022-09-12 17:40:14 -0700

    added comments to import data

--- import_data.py
+++ import_data.py
@@ -2,8 +2,9 @@ import urllib3
 import json
 
 
-# Get Ghibli data from Ghibli API
+# Get Ghibli data from Ghibli API Heroku app 
 http = urllib3.PoolManager()
 r = http.request('GET', 'https://ghibliapi.herokuapp.com/films')
 json_film = json.loads(r.data.decode('utf-8'))
 
+# Print data to data films
