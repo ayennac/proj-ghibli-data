f5cf6e833e8f86cc5c01c0ed1ab683ee5a6f0bd4
ayennac <ayennac@gmail.com>
2022-09-14 17:24:27 -0700

    messing around with styling

--- static/map-script.js
+++ static/map-script.js
@@ -57,24 +57,9 @@ function initMap() {
           shouldFocus:true
         })
       })
-    //to create a map you must initialize with zoom and center
-
+  
   }
 }
 
-  
-// Loop through the results array and place a marker for each
-// set of coordinates.
-// const eqfeed_callback = function (results) {
-//     for (let i = 0; i < results.features.length; i++) {
-//       const coords = results.features[i].geometry.coordinates;
-//       const latLng = new google.maps.LatLng(coords[1], coords[0]);
-  
-//       new google.maps.Marker({
-//         position: latLng,
-//         map: map,
-//       });
-//     }
-//   };
   
 window.initMap = initMap;
