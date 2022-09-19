2f0f97d965f44a6856a1d9b2f01b9b80ea88da96
ayennac <ayennac@gmail.com>
2022-09-19 14:07:52 -0700

    refactored function initMap

--- static/map-script.js
+++ static/map-script.js
@@ -14,7 +14,10 @@ function initMap() {
     fetch('/api/locations')
       .then((response) => response.json())
       .then((locations) => {
+        
         for(const location of locations){
+
+          //Creates all location markers
           const latlong = {lat: location["latitude"], lng: location["longitude"]}
           const marker = new google.maps.Marker({
             position: latlong,
@@ -24,31 +27,21 @@ function initMap() {
           const infowindow = new google.maps.InfoWindow({
             content: location["description"]
           })
-          }
-      });
-   
 
-    
-  //   // for(let i = 0; i < location_data.length; i++){
-  //   //   const latlong = {lat: location_data[i]["latitude"], lng: location_data[i]["longitude"]};
-      
+          const newel = document.createElement('p')
+          newel.textContent = location["name_irl"] +": "+location["description"] 
 
-      
-  //   //   
-      
-  //   //   const newel = document.createElement('p')
-  //   //   newel.textContent = location_data[i]["name_irl"] +": "+location_data[i]["description"] 
-  
-  //   //   marker.addListener("click", () => {
-  //   //     document.getElementById("info-bar").appendChild(newel)
-  //   //     infowindow.open({
-  //   //       anchor:marker,
-  //   //       map,
-  //   //       shouldFocus:true
-  //   //     })
-  //   //   })
-  
-  // }
+
+          marker.addListener("click", () => {
+            document.getElementById("info-bar").appendChild(newel)
+            infowindow.open({
+              anchor:marker,
+              map,
+              shouldFocus:true
+          })
+      })
+    }
+  })
 }
 
   
