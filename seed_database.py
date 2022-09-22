b4dfc1d4461b04bb29b9796b81584b08ec3f5655
ayennac <ayennac@gmail.com>
2022-09-22 12:09:06 -0700

    updated seed_database.py

--- seed_database.py
+++ seed_database.py
@@ -22,22 +22,24 @@ def get_movies():
     with open('data/film_info.json') as f:
         movie_data = json.loads(f.read())
         for movie_to_add in movie_data:
-            model.db.session.add(model.Movie(title = movie_to_add["title"], 
-                                original_title = movie_to_add["original_title"],
-                                original_title_rom = movie_to_add["original_title_romanised"],
-                                image = movie_to_add["image"],
-                                movie_banner = movie_to_add["movie_banner"],
-                                description = movie_to_add["description"],
-                                director = movie_to_add["director"],
-                                producer = movie_to_add["producer"],
-                                release_date = movie_to_add["release_date"],
-                                running_time = movie_to_add["running_time"],
-                                rt_score = movie_to_add["rt_score"]
-                                #add all the new stuff
-                                ))
+            new_movie = crud.create_new_movie(movie_to_add["title"],
+                                    movie_to_add["original_title"],
+                                    movie_to_add["original_title_romanised"],
+                                    movie_to_add["image"],
+                                    movie_to_add["movie_banner"],
+                                    movie_to_add["description"],
+                                    movie_to_add["director"],
+                                    movie_to_add["producer"],
+                                    movie_to_add["release_date"],
+                                    movie_to_add["running_time"],
+                                    movie_to_add["rt_score"]
+                                    )
+            model.db.session.add(new_movie)
     model.db.session.commit()
 
-
+studio_ghibli = crud.create_new_user("StudioGhibli", "Studio", "Ghibli", "StudioGhibli@StudioGhibli.com", "StudioGhibli")
+model.db.session.add(studio_ghibli)
+model.db.session.commit()
 
 def get_location():
     """Load location from dataset into database."""
@@ -45,13 +47,20 @@ def get_location():
         location_data = json.loads(r.read())
         for location in location_data:
             movie_to_edit = crud.get_movie_from_title(location["movie"])
-            model.db.session.add(model.Location(image = location["image"],
-                                    latitude = location["latitude"],
-                                    longitude = location["longitude"],
-                                    place_movie = location["place_movie"],
-                                    name_irl = location["name_irl"],
-                                    description = location["description"],
-                                    movie = movie_to_edit))    
+            ghibli_user = crud.get_user_by_username("StudioGhibli")
+            new_location = crud.create_new_location(ghibli_user,
+                                                    movie_to_edit,
+                                                    location["latitude"],
+                                                    location["longitude"],
+                                                    location["place_movie"],
+                                                    location["name_irl"],
+                                                    location["description"],
+                                                    location["photo"],
+                                                    location["movie_still"],
+                                                    False, 
+                                                    "Approved",
+                                                    True)    
+            model.db.session.add(new_location)    
     model.db.session.commit()
             
 
